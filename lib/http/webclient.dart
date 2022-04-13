458c3118e92fd6a148c76cd216ce8281d6790344
JheimisonFLX <jheimison_007@hotmail.com>
2022-04-13 13:46:06 -0400

    Aula02 - 06

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,8 +1,34 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
 
 void findAll() async {
+
   final url = Uri.http('192.168.100.21:8080', 'transactions');
 
-  final Response response = await get(url);
+  Client client = InterceptedClient.build(interceptors: [LoggingInterceptor()]);
+
+  final Response response = await client.get(url);
   print(response.body);
 }
+
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    print('Request');
+    print('url: ${data.url}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    print('Response');
+    print('status code: ${data.statusCode}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+}
+
